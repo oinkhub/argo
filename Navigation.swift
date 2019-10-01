e3b6d414ba88665f51bd76537c4bcd7f30ecf010
vauxhall <zero.griffin@gmail.com>
2019-10-01 15:48:41 +0200

    Update folder

---
+++ Navigation.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct Navigation: View {
+    @ObservedObject var places: Places
+    var item: Session.Item
+    
+    var body: some View {
+        ZStack {
+            Image("heading")
+            
+            GeometryReader { geo in
+                ZStack {
+                    ForEach((0...144), id: \.self) { p in
+                        Path {
+                            let side = min(geo.size.width, geo.size.height) * 0.485
+                            $0.move(to: .init(x: geo.size.width / 2, y: (geo.size.height / 2) - side - 2))
+                            $0.addLine(to: .init(x: geo.size.width / 2, y: (geo.size.height / 2) - side + (p == 0 ? 10 : 2)))
+                        }.stroke(Color("halo"), style: .init(lineWidth: p == 0 ? 3 : 1, lineCap: .round)).rotationEffect(.degrees(Double(p) * 2.5))
+                    }
+                }
+            }
+            .rotationEffect(.degrees(places.heading)).animation(.easeOut)
+            .navigationBarTitle(item.name)
+        }
+    }
+}
